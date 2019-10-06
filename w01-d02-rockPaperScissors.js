9087d7cb64a395c7e4801486aa34ee83ec4c7fb6
unknown <hadil.a.hassouna@gmail.com>
2019-10-05 23:42:40 -0700

    toy prob

--- w01-d02-rockPaperScissors.js
+++ w01-d02-rockPaperScissors.js
@@ -18,7 +18,19 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function () {
+	var output = [];
+	var player;
+	var R1 = "R";
+	var R2 = "P";
+	var R3 = "S";
+	var round = [R1, R2, R3];
+	//for (i=0; )
+		// Number of possibilities = 3*3*3 = 27,
+	// each time fixing two rounds and change the third one ,
+	//tell all the possibilities finished
+		return "" + R1 + R2 + R3 + "";
+
+	return output.puch(round);
   // TODO: your solution here
 };
